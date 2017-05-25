1995d23de72063797361d48f7854f69be6f18cc1
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-05-25 18:49:59 +0900

    Use joda-time just for logging on the plugin side.

--- independent-plugin/src/main/java/IndependentPlugin.java
+++ independent-plugin/src/main/java/IndependentPlugin.java
@@ -1,6 +1,15 @@
+import org.joda.time.DateTime;
+
 public class IndependentPlugin implements PluginSpi {
     @Override
     public boolean isDifferenceLongerThanDay(Timestamp x, Timestamp y) {
-        throw new UnsupportedOperationException("Not implemented.");
+        // Regardless of the implementation of Timestamp, using joda-time just for logging.
+        System.out.println(DateTime.now().toString());
+
+        final long absoluteDiff;
+        if (x.getEpoch() < y.getEpoch()) { absoluteDiff = y.getEpoch() - x.getEpoch(); }
+        else                             { absoluteDiff = x.getEpoch() - y.getEpoch(); }
+
+        return absoluteDiff >= 60L * 60L * 24L;
     }
 }
