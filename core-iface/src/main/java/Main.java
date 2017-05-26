9abcaaef745c512de5cc096d3f3faa5923fb45f7
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-05-26 16:09:32 +0900

    Load library jars and create instances. (failing)

--- core-iface/src/main/java/Main.java
+++ core-iface/src/main/java/Main.java
@@ -1,5 +1,62 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+        JarClassLoader loader = new JarClassLoader(
+            ((URLClassLoader) ClassLoader.getSystemClassLoader()).getURLs(),
+            Thread.currentThread().getContextClassLoader());
+        final Class<?> timestampImplClass;
+        try {
+            timestampImplClass = loader.loadClass("TimestampImpl");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            return;
+        }
+        System.out.println(timestampImplClass.toString());
+
+        final Class<?> jodaDateTimeClass;
+        try {
+            jodaDateTimeClass = loader.loadClass("org.joda.time.DateTime");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            return;
+        }
+        System.out.println(jodaDateTimeClass.toString());
+
+        final Method nowMethod;
+        try {
+            nowMethod = jodaDateTimeClass.getMethod("now");
+        } catch (NoSuchMethodException ex) {
+            ex.printStackTrace();
+            return;
+        }
+        System.out.println(nowMethod.toString());
+
+        final Constructor timestampConstructor;
+        try {
+            timestampConstructor = timestampImplClass.getConstructor(jodaDateTimeClass);
+        } catch (NoSuchMethodException ex) {
+            ex.printStackTrace();
+            return;
+        }
+        System.out.println(timestampConstructor.toString());
+
+        final Timestamp timestamp;
+        try {
+            timestamp = (Timestamp) timestampConstructor.newInstance(nowMethod.invoke(null));
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+            return;
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+            return;
+        } catch (InvocationTargetException ex) {
+            ex.printStackTrace();
+            return;
+        }
     }
 }
